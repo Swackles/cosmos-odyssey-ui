3d2065b3e19e7394b5ab3e3d578cf634180ba121
Swackles <Swackles@users.noreply.github.com>
2021-04-26 20:49:26 +0300

    init commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/" />
+        <Route path='/pricingLists' />
+        <Route path='/reservations' />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
