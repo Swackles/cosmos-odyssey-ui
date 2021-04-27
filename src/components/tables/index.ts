93c306d391e01c2bc6c238e8556e60731242dac0
Swackles <Swackles@users.noreply.github.com>
2021-04-27 23:14:55 +0300

    added reservations table

--- src/components/tables/index.ts
+++ src/components/tables/index.ts
@@ -1,2 +1,3 @@
 export { default as PriceListingsTable } from './priceListingsTable'
 export { default as ProvidersTable } from './providersTable'
+export { default as ReservationsListingsTable } from './reservationsListingsTable'
---
+++ src/components/tables/reservationsListingsTable.tsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import { DataGrid, GridCellParams, GridColDef } from '@material-ui/data-grid';
+import { Button } from '@material-ui/core';
+import { durationString } from './../../lib'
+
+interface PriceListing {
+  dest: string
+  distance: string
+  endTime: Date
+  id: number
+  origin: string
+  price: number
+  startTime: Date
+  duration: string
+}
+
+interface Data {
+  id: number,
+  firstName: string,
+  lastName: string,
+  priceListing: PriceListing
+  fullName?: string
+  endTime?: Date
+  startTime?: Date
+  duration?: string
+}
+interface Input {
+  onView: (id: number) => void
+  data: Data[]
+}
+
+class PriceListingsTable extends React.Component<Input> {
+  public render(): JSX.Element {
+    let { onView, data, } = this.props
+
+    data = data.map(x => {
+      x.fullName = `${x.firstName} ${x.lastName}`
+
+      x.startTime = new Date(x.priceListing.startTime)
+      x.endTime = new Date(x.priceListing.endTime)
+
+      x.duration = durationString(x.endTime.getTime() - x.startTime.getTime())
+      return x
+    })
+
+    console.log(data)
+
+    let columns: GridColDef[] = [
+      { field: 'id', headerName: 'ID', type: 'number', flex: .8 },
+      { field: 'fullName', headerName: 'Full name', flex: 2 },
+      { field: 'startTime', headerName: 'Departing time', type: 'dateTime', flex: 2 },
+      { field: 'endTime', headerName: 'Arrival Time', type: 'dateTime', flex: 2 },
+      { field: 'duration', headerName: 'Duration', sortable: false, type: 'string', flex: 2.5 },
+      { field: '', headerName: '', sortable: false, flex: 1.5,
+        renderCell: (params: GridCellParams) => (
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+            style={{ marginLeft: 16 }}
+            onClick={() => { onView(parseInt(params.getValue('id') as string)) }} > View </Button>
+        )
+      }
+    ]
+
+    return (
+      <div style={{ height: 600, width: '100%', minWidth: 300 }}>
+        <DataGrid
+          disableSelectionOnClick={true}
+          loading={data === []}
+          rows={data}
+          columns={columns}
+          pageSize={15}
+          disableColumnMenu={true} />
+      </div>
+    )
+  }
+}
+
+export default PriceListingsTable
